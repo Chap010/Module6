ba5e88309f1ec0478cf7ac47a950e56a61d62991
Antonio <atejda8071@southwestuniversity.edu>
2021-06-03 19:24:46 -0600

    Fixed Module 5 Assignment

---
+++ 131Module5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _131Module5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int valor;
+            valor = 0;
+            
+            for (int sumation = 1; sumation < 21; sumation++) // 
+            {
+                valor = valor + sumation; // suma 
+            }
+
+            Console.WriteLine(valor);
+            
+
+        }
+
+    }
+
+}
